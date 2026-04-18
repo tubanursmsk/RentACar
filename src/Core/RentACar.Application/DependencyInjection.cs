93a88ce8e4741721aeedcd89a481d56514b4541d
tubanursmsk <tubanursmsk@gmail.com>
2026-04-18 12:17:03 +0300

    .

--- src/Core/RentACar.Application/DependencyInjection.cs
+++ src/Core/RentACar.Application/DependencyInjection.cs
@@ -7,10 +7,8 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
-        // AutoMapper v13+ kullanımı
+        // Modern AutoMapper v13 kullanımı
         services.AddAutoMapper(Assembly.GetExecutingAssembly());
-        
-        // Diğer kurgular (FluentValidation vb.) buraya gelecek
         
         return services;
     }

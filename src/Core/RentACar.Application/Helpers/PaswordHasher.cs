18d83918dd18facef9150f44c97ab0ebcf83f250
tubanursmsk <tubanursmsk@gmail.com>
2026-04-20 11:37:52 +0300

    tuzlama

--- src/Core/RentACar.Application/Helpers/PaswordHasher.cs
+++ src/Core/RentACar.Application/Helpers/PaswordHasher.cs
@@ -4,13 +4,13 @@ namespace RentACar.Application.Helpers
 {
     public static class PasswordHasher
     {
-        // Şifreyi Hash'ler (Geri döndürülemez formata çevirir)
+        // Şifreyi Hash'ler
         public static string HashPassword(string password)
         {
             return BCrypt.Net.BCrypt.HashPassword(password);
         }
 
-        // Girilen şifre ile veritabanındaki Hash'li şifreyi karşılaştırır
+        // Girilen şifre ile veritabanındaki Hash'li şifreyi karşılaştır
         public static bool VerifyPassword(string password, string hashedPassword)
         {
             return BCrypt.Net.BCrypt.Verify(password, hashedPassword);
